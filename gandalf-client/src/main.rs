f49f57eff36db699e4e5bb3cd6e08a01b3c59355
Jimmy <miki3867@gmail.com>
2022-03-21 09:13:45 +0100

    Setup server

--- gandalf-client/src/main.rs
+++ gandalf-client/src/main.rs
@@ -1,42 +1,42 @@
 use std::net::UdpSocket;
 use std::str;
+use std::io;
+use webbrowser;
+
 
 fn main() {
     let connection_string:String = String::from("0.0.0.0:8000");
+    let socket = connect(connection_string)
+        .expect("Cannot connect bind to port");
 
-    let socket = connect(connection_string);
-    handle_communication(socket);
+    handle_communication(socket)
+        .map_err(|err| println!("{:?}", err)).ok(); 
 }
 
-fn connect(connection_string: String) -> UdpSocket {
-    let socket = match UdpSocket::bind(connection_string) {
-        Ok(s) => s,
-        Err(e) => panic!("{}", e)
-    };
-    socket
+fn connect(connection_string: String) -> Result<UdpSocket, io::Error> {
+    let socket =  UdpSocket::bind(connection_string)?; 
+    Ok(socket)
 }
 
-fn handle_communication(socket: UdpSocket) {
-    let mut buf = [0; 2048];
-
+fn handle_communication(socket: UdpSocket) -> std::io::Result<()> {
     loop {
-        let msg = match socket.recv_from(&mut buf) {
-            Ok((amt, src)) =>{ 
-                str::from_utf8(&buf[..amt]).unwrap_or("")
-            }
-            Err(e) => {
-                println!("couldn't recieve a datagram: {}", e);
-                "a"
-            }
-        };
+        let mut buf = [0; 2048];
+        let (amt, src) = socket.recv_from(&mut buf)?; 
+        let msg = str::from_utf8(&buf[..amt]).unwrap_or("");
+        
+
+        println!("{}", msg);
 
         let msg: Vec<&str>= msg.split_whitespace().collect();
+
         let command = msg[0];
         let url = msg[1];
-
+        
         match command {
             "web" => {
-                println!("Czuwaj to stara kurwa");
+                if !webbrowser::open(url).is_ok() {
+                    panic!("Cannot open a webbrowser");
+                }
             },
             _ => {
             }
