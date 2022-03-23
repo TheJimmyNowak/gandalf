a455721f3711d38880364d5b49c78499c73ff883
Jimmy <miki3867@gmail.com>
2022-03-23 17:42:16 +0100

    Comm between backend and client

---
+++ gandalf-backend/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import socket
+
+app = Flask(__name__)
+SERVER_ADDRESS_PORT = ("0.0.0.0", 8000)
+
+client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+@app.route("/web/", methods=['POST'])
+def web():
+    url = request.json['url']
+    message = f"web {url}"
+    encoded_message = str.encode(message)
+    client_socket.sendto(encoded_message, SERVER_ADDRESS_PORT)
+    return "Succes"
--- gandalf-client/src/main.rs
+++ gandalf-client/src/main.rs
@@ -6,8 +6,11 @@ use webbrowser;
 
 fn main() {
     let connection_string:String = String::from("0.0.0.0:8000");
+    println!("Connecting to: {}", connection_string); 
+
     let socket = connect(connection_string)
         .expect("Cannot connect bind to port");
+    println!("Server is running!");
 
     handle_communication(socket)
         .map_err(|err| println!("{:?}", err)).ok(); 
@@ -25,15 +28,17 @@ fn handle_communication(socket: UdpSocket) -> std::io::Result<()> {
         let msg = str::from_utf8(&buf[..amt]).unwrap_or("");
         
 
-        println!("{}", msg);
+        println!("We have message: {}", msg);
 
         let msg: Vec<&str>= msg.split_whitespace().collect();
 
+        println!("Message splitted");
         let command = msg[0];
         let url = msg[1];
         
         match command {
             "web" => {
+                println!("Opening browser");
                 if !webbrowser::open(url).is_ok() {
                     panic!("Cannot open a webbrowser");
                 }
