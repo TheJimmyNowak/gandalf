1d7a6acf9ec26fe6228ae0df5385eac6a3d09148
Jimmy <miki3867@gmail.com>
2022-03-31 19:18:38 +0200

    Create build.sh and deploy

--- gandalf-frontend/src/App.js
+++ gandalf-frontend/src/App.js
@@ -5,6 +5,17 @@ import axios from 'axios';
 function App() {
   const API_ADDRESS="http://127.0.0.1:5000";
   const [inputs, setInputs] = useState({});
+  const clients = []
+
+  var i = 0;
+  // Hardcoded clients cause clients has static ip
+  for (i=0; i<18; i++){
+    clients.push("172.16.110." + i);
+  }
+
+  const listClients = clients.map((client) =>
+    <li key={client}>{client}</li>
+  )
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -15,20 +26,29 @@ function App() {
   const handleWebSubmit = (event) => {
     event.preventDefault();
     axios.post(API_ADDRESS + "/web/", {
-      url: inputs.url 
+      url: inputs.url,
+      host: inputs.host
     })
     .then((response) => {
     }, (error) => {
       alert(error)
     });
   }
+  
   return (
     <div id="container">
       <form onSubmit={handleWebSubmit}>
-        <label for="url">Url: </label><br/>
+        <label>Url: </label>
         <input type="url" name="url" id="url" pattern="https://.*" onChange={handleChange} required/><br/>
+        <label>Host: </label>
+        <input name="host" id="host" onChange={handleChange}/><br/>
         <input type="submit" value="Open web"/><br/>
       </form>
+      <div id="clients-list">
+        {
+          listClients
+        } 
+      </div>
     </div>
   );
 }
